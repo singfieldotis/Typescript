7e51ede764c6fda8ee2a75309acf8098d8debc28
Otis Singfield <singfieldotis@gmail.com>
2021-08-11 01:26:28 -0400

    Day 1

---
+++ .history/fetchjson/index_20210811005825.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => )
---
+++ .history/fetchjson/index_20210811005838.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+  console.log(response.data);
+
+})
